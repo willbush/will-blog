18a84d2ac0d22a47498fce6326b11e509df14146
William Bush <william.bush@utdallas.edu>
2016-01-01 23:14:28 -0600

    add database versioning with Fluent

---
+++ will-blog/Migrations/Migration001_UsersAndRoles.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using FluentMigrator;
+
+namespace will_blog.Migrations
+{
+    [Migration(1)]
+    public class Migration001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                  .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                  .WithColumn("username").AsString(128)
+                  .WithColumn("email").AsCustom("VARCHAR(256)")
+                  .WithColumn("password_hash").AsString(128);
+
+            Create.Table("roles")
+                  .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                  .WithColumn("name").AsString(128);
+
+            Create.Table("roles_users")
+                  .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                  .WithColumn("role_id").AsInt32().ForeignKey("roles", "id").OnDelete(Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("roles_users");
+            Delete.Table("roles");
+            Delete.Table("users");
+        }
+    }
+}
